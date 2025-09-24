d1ac8364ed86969fd4648af60d6f689866b750a4
msc2024T <mohamad.sc66@gmail.com>
2025-09-24 17:26:34 -0400

    Filters & Lookups

--- shop/queries.py
+++ shop/queries.py
@@ -130,3 +130,46 @@ def addOrderItem(ordeId: int, productId: int, quantity: int):
         order=order, product=product, quantity=quantity)
     print(result)
     return result
+
+
+# Filters & Lookups
+def getProductsCheaperThan(price: Decimal):
+
+    products = Product.objects.filter(price__lt=price)
+    result = dict(total=products.count(), product=products)
+    print(result)
+    return result
+
+
+def searchProductsByName(keyword: str):
+
+    products = Product.objects.filter(name__icontains=keyword)
+    result = dict(total=products.count(), products=products)
+
+    print(result)
+    return result
+
+
+def getOrdersByYear(year: int):
+
+    orders = Order.objects.filter(created_at__year=year)
+    result = dict(total=orders.count(), orders=orders)
+
+    print(result)
+    return result
+
+
+def excludeCustomer(email: str):
+
+    customers = Customer.objects.all().exclude(email=email)
+    result = dict(total=customers.count(), customers=customers)
+
+    print(result)
+    return result
+
+
+def getLatestOrder():
+
+    order = Order.objects.all().order_by('created_at').last()
+    print(order)
+    return order
