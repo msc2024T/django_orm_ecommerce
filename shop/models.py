269be39523284c5d7d02b733965c499dbb123c3a
msc2024T <mohamad.sc66@gmail.com>
2025-09-24 16:41:52 -0400

    basic crud

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f" name:{self.name} email: {self.email}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"product name : {self.name}"
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name='orders')
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.name}"
+
+
+class OrderItem(models.Model):
+
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='orderItems')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='orderItems')
+    quantity = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('order', 'product')
+
+    def __str__(self):
+        return f'order item id: #{self.id} order id: #{self.order.id} product name: {self.product.name}'
---
+++ shop/queries.py
@@ -0,0 +1,132 @@
+from .models import Customer, Order, OrderItem, Product
+from decimal import Decimal
+
+# crud basics - create
+
+
+def addCustomer(name: str, email: str):
+
+    newCustomer = Customer.objects.create(name=name, email=email)
+    print(newCustomer)
+
+
+def getCustomerList():
+
+    customers = Customer.objects.all()
+    result = dict()
+    result['total'] = customers.count()
+    result['list'] = customers
+
+    print(result)
+
+
+def getCustomer(id: int):
+
+    customer = Customer.objects.get(id=id)
+    print(customer)
+    return customer
+
+
+def addProduct(name: str, price: Decimal, stock: int):
+
+    result = Product.objects.create(
+        name=name, price=price, stock=stock)
+    print(result)
+
+
+def getProductList():
+
+    products = Product.objects.all()
+    result = dict(total=products.count(), list=list)
+
+    print(result)
+    return result
+
+
+def getProduct(productId: int):
+
+    product = Product.objects.get(id=productId)
+    print(product)
+    return product
+
+
+def updateProductPrice(productId: int, price: Decimal):
+
+    product = getProduct(productId)
+
+    product.price = price
+    product.save()
+
+    print(product)
+    return product
+
+
+def decreaseProductStock(productId: int):
+    try:
+        product = getProduct(productId)
+        if product.stock > 0:
+            product.stock -= 1
+            product.save()
+            print(f"Stock decreased. New stock: {product.stock}")
+            return product
+        else:
+            print(f"Product with id {productId} is out of stock.")
+            return None
+    except Product.DoesNotExist:
+        print(f"Product with id {productId} does not exist.")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
+
+
+def addOrder(customerId: int):
+
+    customer = getCustomer(customerId)
+
+    result = Order.objects.create(customer=customer)
+
+    print(result)
+    return (result)
+
+
+def getOrderList():
+
+    orders = Order.objects.all()
+    result = dict(total=orders.count(), orders=orders)
+    print(result)
+
+    return result
+
+
+def getOrder(orderId: int):
+
+    order = Order.objects.get(id=orderId)
+    print(order)
+    return order
+
+
+def deleteOrder(orderId: int):
+
+    try:
+        order = getOrder(orderId=orderId)
+        order.delete()
+        print('deleted')
+        return True
+    except Order.DoesNotExist:
+        print(f'Order with Id {orderId} Not Found')
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
+
+
+def addOrderItem(ordeId: int, productId: int, quantity: int):
+
+    order = getOrder(orderId=ordeId)
+    product = getProduct(productId=productId)
+
+    result = OrderItem.objects.create(
+        order=order, product=product, quantity=quantity)
+    print(result)
+    return result
